cf89bf9f86cd411f1fcbae294428af95170dfeed
Andre Bogus <bogusandre@gmail.com>
2016-11-20 16:38:16 +0100

    convert benchmarks to bluss' bencher

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,6 +1,6 @@
-#![feature(test)]
 
-extern crate test;
+#[macro_use]
+extern crate bencher;
 extern crate rand;
 extern crate bytecount;
 
@@ -14,14 +14,12 @@ fn random_bytes(len: usize) -> Vec<u8> {
 
 macro_rules! bench {
     ($i: expr, $name_naive: ident, $name_hyper: ident) => {
-        #[bench]
-        fn $name_naive(b: &mut test::Bencher) {
+        fn $name_naive(b: &mut bencher::Bencher) {
             let haystack = random_bytes($i);
             b.iter(|| naive_count(&haystack, 10));
         }
 
-        #[bench]
-        fn $name_hyper(b: &mut test::Bencher) {
+        fn $name_hyper(b: &mut bencher::Bencher) {
             let haystack = random_bytes($i);
             b.iter(|| count(&haystack, 10));
         }
@@ -36,3 +34,14 @@ bench!(1000, bench_1000_naive, bench_1000_hyper);
 bench!(10000, bench_10000_naive, bench_10000_hyper);
 bench!(100000, bench_100000_naive, bench_100000_hyper);
 bench!(1000000, bench_1000000_naive, bench_1000000_hyper);
+
+benchmark_group!(bench, bench_0_naive, bench_0_hyper,
+                        bench_1_naive, bench_1_hyper,
+                        bench_10_naive, bench_10_hyper,
+                        bench_100_naive, bench_100_hyper,
+                        bench_1000_naive, bench_1000_hyper,
+                        bench_10000_naive, bench_10000_hyper,
+                        bench_100000_naive, bench_100000_hyper,
+                        bench_1000000_naive, bench_1000000_hyper);
+                        
+benchmark_main!(bench);
